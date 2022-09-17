58d48b784dafb55f1ea2e3ab991259a32acbb9d4
Joshua Harsono <jharsono@gmail.com>
2022-09-17 00:37:42 -0700

    Add json stringify

--- scripts/get-tailwind-colors/get-tailwind-colors.js
+++ scripts/get-tailwind-colors/get-tailwind-colors.js
@@ -8,35 +8,29 @@ config({ path: '../../.env' });
 // Extract PAT and Workspace from .env
 const { PERSONAL_ACCESS_TOKEN } = process.env;
 
-// use Commander to take in options from the command line
+// Use Commander to take in options from the command line
 const program = new Command();
 
-// Instantiate zeplin with access token, add our http client to the zeplin
+// Instantiate zeplin with access token
 
 const zeplin = new ZeplinApi(new Configuration({ accessToken: PERSONAL_ACCESS_TOKEN }));
 
-const getStyleguideColors = async (styleguideId) => {
-  const { data } = await zeplin.colors.getStyleguideColors(styleguideId);
-  const colors = data.reduce((prev, color) => {
-    const {
-      name,
-      r,
-      g,
-      b,
-    } = color;
+const getDesignTokenColors = async (styleguideId) => {
+  const { data: { colors } } = await zeplin.designTokens.getStyleguideDesignTokens(styleguideId);
 
-    return { ...prev, [name.toLowerCase()]: `rgb(${r},${g},${b})` };
-  }, {});
-
-  return JSON.stringify(colors, null, 2);
+  // Format colors object for use in Tailwind config
+  return Object.entries(colors).reduce((prev, [color, { value }]) => (
+    { ...prev, [color]: value }
+  ), {});
 };
 
 program
   .requiredOption('-s, --styleguideId <styleguideId>', 'Styleguide ID')
   .action(async ({ styleguideId }) => {
-    const colors = await getStyleguideColors(styleguideId);
+    const colors = await getDesignTokenColors(styleguideId);
 
-    console.log(colors);
+    // Present results to command line for manual copy into Tailwind config
+    console.log(JSON.stringify(colors, null, 2));
   });
 
 program.parseAsync(process.argv);
